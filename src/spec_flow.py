887a0fe8d0c42ad9f181fcfe3c59d58f29db8856
generatedunixname537391475639613 <noreply+537391475639613@fb.com>
2025-10-29 20:27:55 -0700

    fbcode/infra_asic_fpga/common/tools/veripy/open_source/src/spec_flow.py
    Differential Revision: D85658682
    
    fbshipit-source-id: c5ed85aee170f77686d14e4c5f609d98e9b7e2b4

--- src/spec_flow.py
+++ src/spec_flow.py
@@ -26,12 +26,11 @@ import os
 import os.path
 import re
 import sys
-
-from src.utils import *
-from src.regex import *
 from csv import reader
 
 import yaml as yaml
+
+from src.regex import RE_EQUAL_EXTRACT, RE_IF_PREFIX_ITER_CHECK, RE_IF_SUFFIX_ITER_CHECK
 
 
 ################################################################################
